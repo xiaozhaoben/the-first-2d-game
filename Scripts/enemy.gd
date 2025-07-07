eb34c1023af0241c334e0bae04ec261e40ad40f8
hzt <2502712438@qq.com>
2025-07-07 22:27:38 +0800

    feat: 增加随机史莱姆

--- Scripts/enemy.gd
+++ Scripts/enemy.gd
@@ -2,12 +2,24 @@ extends Area2D
 
 @export var slime_speed : float = -100
 
+var is_dead : bool = false
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta: float) -> void:
-	position += Vector2(-100, 0) * delta
+	if !is_dead:
+		position += Vector2(-100, 0) * delta
 
 
 func _on_body_entered(body: Node2D) -> void:
 	if body is CharacterBody2D:
 		print("hit Player, game over")
 		body.game_over()
+
+func _on_area_entered(area: Area2D) -> void:
+	if area.is_in_group("bullet"):
+		$AnimatedSprite2D.play("death")
+		is_dead = true
+		area.queue_free()
+		await get_tree().create_timer(0.6).timeout
+		queue_free()
+	
---
+++ Scripts/gameManager.gd
@@ -0,0 +1,19 @@
+extends Node2D
+
+@export var slime_scene : PackedScene
+@export var spawn_timer : Timer
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	spawn_timer.wait_time -= 0.2 * delta
+	spawn_timer.wait_time = clamp(spawn_timer.wait_time, 1, 3)
+
+
+func _spawn_slime() -> void:
+	var slime_node = slime_scene.instantiate()
+	slime_node.position = Vector2(257, randf_range(54, 106))
+	get_tree().current_scene.add_child(slime_node)
