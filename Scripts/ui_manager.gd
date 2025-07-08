b5a3e15d5bbbaeb86c037023222b0057ebef317c
hzt <2502712438@qq.com>
2025-07-08 15:35:41 +0800

    修复UIManager语法错误

--- Scripts/ui_manager.gd
+++ Scripts/ui_manager.gd
@@ -3,3 +3,6 @@ extends Control
 # Add to ui_manager group for easier finding
 
 func _ready() -> void:
+	# Add this node to the ui_manager group for easier finding
+	add_to_group("ui_manager")
+	print("UIManager ready and added to group")
